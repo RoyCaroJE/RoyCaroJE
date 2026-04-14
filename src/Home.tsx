f5911fadbd1cb2708acf1ba3424aa1f2ace55cfc
RoyCaroJE <roycaroje@gmail.com>
2026-04-14 17:53:23 +0200

    Remove Suspense and Lazy Loading

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,21 +1,15 @@
-import { lazy, Suspense } from "react";
 import { Container, Divider, Stack } from "@mui/material";
 import Presentation from "./components/Presentation";
-
-const Projects = lazy(() => import("./components/projects/Projects.tsx"));
-const Experience = lazy(() => import("./components/experience/Experience.tsx"));
+import Experience from "./components/experience/Experience";
+import Projects from "./components/projects/Projects";
 
 function Home() {
   return (
     <Container>
       <Stack divider={<Divider />} spacing={2}>
         <Presentation />
-        <Suspense fallback={null}>
-          <Projects />
-        </Suspense>
-        <Suspense fallback={null}>
-          <Experience />
-        </Suspense>
+        <Projects />
+        <Experience />
       </Stack>
     </Container>
   );
